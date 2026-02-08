3d71d385f2e5babe59c8e32c9564ce9980328fb4
mrmueez55 <mrmueez55@gmail.com>
2026-02-09 00:37:14 +0500

    Fix postcss config for Vercel deployment

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,6 @@
-const config = {
-  // plugins: {
-  //   "@tailwindcss/postcss": {},
-  // },
-    plugins: {
+module.exports = {
+  plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
-};
-
-export default config;
+}
